4d4d01c3b53ccd2be59581cb25116a06847bf4be
msegreto <msegreto1202@gmail.com>
2025-04-05 12:12:36 +0200

    initial commit

---
+++ notebook/utils/text_preprocessing.py
@@ -0,0 +1,39 @@
+import re
+import emoji
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem import SnowballStemmer
+
+stop_words = set(stopwords.words('english'))
+stemmer = SnowballStemmer("english")
+tokenizer = RegexpTokenizer(r'\b[a-z]{2,}\b')  # solo parole con almeno 2 lettere
+
+def preprocess_text(text):
+    # Lowercase
+    text = text.lower()
+    
+    # URL removal
+    text = re.sub(r"http\S+|www\S+|https\S+", '', text, flags=re.MULTILINE)
+    
+    # Emoji removal
+    text = emoji.replace_emoji(text, replace='')
+
+    # tag & hashtag removal
+    text = re.sub(r'@\w+|#', '', text)
+    
+    # punctuation removal
+    text = re.sub(r'[^\w\s]', '', text)
+    
+    # number removal
+    text = re.sub(r'\d+', '', text)
+    
+    # Tokenization (regex-based)
+    tokens = tokenizer.tokenize(text)
+    
+    # Stopword removal + stemming
+    processed = [
+        stemmer.stem(word) for word in tokens if word not in stop_words and word.isalpha()
+    ]
+    
+    return ' '.join(processed)
+
