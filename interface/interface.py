6719c600b2fb1c412521d718bf9db1899fec1197
msegreto <msegreto1202@gmail.com>
2025-05-20 09:47:41 +0200

    changed dataset, added nested CV, Wilcoxon & t-tests, binary explainability module, updated docs

--- interface/interface.py
+++ interface/interface.py
@@ -16,16 +16,14 @@ from utils.text_preprocessing import preprocess_text, TextPreprocessor #Run time
 from utils.bad_word_blurrer import blur_text
 from utils.two_stage_classifier import TwoStageClassifier
 
-binary_pipeline = load("../model/final_pipeline_binary.pkl")
-multiclass_pipeline = load("../model/final_pipeline_multiclass.pkl")
+multiclass_pipeline = load("../model/multiclass/RandomForest_TF-IDF_multiclass.pkl")
+binary_pipeline= load("../model/binary/RandomForest_TF-IDF.pkl")
 
 
 label_map = {
-    0: "age",
-    1: "ethnicity",
-    2: "gender",
-    3: "other_cyberbullying",
-    4: "religion"
+    0: "ethnicity/race",
+    1: "gender/sexual",
+    2: "religion",
 }
 
 pipeline_2stage = Pipeline([
@@ -89,7 +87,9 @@ def explanation_window(predicted_label, preprocessed):
     image_frame = tk.Frame(right_frame)
     image_frame.pack(side=tk.TOP, pady=10)
 
-    image_filename = f"top_25_{predicted_label}.png"
+    print(predicted_label)
+    safe_class = predicted_label.replace("/", "-")
+    image_filename = f"top_25_{safe_class}.png"
     image_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "word_class_distribution", image_filename))
 
     if os.path.isfile(image_path):
